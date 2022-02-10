e22c98f4674dd1eb9a4b6ff46582cdc2b97a61b5
Sam Deane <sam@elegantchaos.com>
2022-02-10 20:19:48 +0000

    Script bits

---
+++ Sources/SkyrimFileFormat/FieldTypes/ScriptsField.swift
@@ -0,0 +1,28 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 10/02/22.
+//  All code (c) 2022 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+
+struct ScriptsField: Codable {
+    let version: Int16
+    let objFormat: Int16
+    let scriptCount: UInt16
+
+    struct Script: Codable {
+        let name: String
+        let status: UInt8
+        let propertyCount: UInt16
+        let properties: [Property]
+    }
+
+    struct Property: Codable {
+        let name: String
+        let type: UInt8
+        let status: UInt8
+        
+    }
+
+}
+
