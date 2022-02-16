4223174de93b3a60642311775b5645b450e9b08d
Sam Deane <sam@elegantchaos.com>
2022-02-16 15:15:02 +0000

    Updated tests

--- Tests/SkyrimFileFormatTests/ESPJSONDecodingTests.swift
+++ Tests/SkyrimFileFormatTests/ESPJSONDecodingTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 
 class ESPJSONDecodingTests: ProcessorTestCase {
-    func loadESPS(named name: String) async throws -> ESPS {
+    func loadESPS(named name: String) async throws -> ESPBundle {
         let url = Bundle.module.url(forResource: "Unpacked/\(name)", withExtension: "esps")!
         return try processor.loadESPS(url)
     }
@@ -19,15 +19,21 @@ class ESPJSONDecodingTests: ProcessorTestCase {
         let records = try await loadESPS(named: "Empty")
         XCTAssertEqual(records.count, 1)
         let header = records.header
-        XCTAssertEqual(header?.tag, TES4Record.tag)
+        XCTAssertEqual(header?.type, TES4Record.tag)
         XCTAssertEqual(header?.info.version, 1.7)
     }
     
     func testArmour() async throws {
         let records = try await loadESPS(named: "Armour")
         XCTAssertEqual(records.count, 11)
+        XCTAssertEqual(records.index[ARMORecord.tag]?.count, 1)
+        XCTAssertEqual(records.index[ARMARecord.tag]?.count, 1)
+        XCTAssertEqual(records.index[LVLIRecord.tag]?.count, 3)
+        XCTAssertEqual(records.index[OTFTRecord.tag]?.count, 1)
+        XCTAssertEqual(records.index[TXSTRecord.tag]?.count, 1)
+
         let header = records.header
-        XCTAssertEqual(header?.tag, TES4Record.tag)
+        XCTAssertEqual(header?.type, TES4Record.tag)
         XCTAssertEqual(header?.info.version, 1.7)
     }
 }
