80654fb2735e494293ac88f00a981e81bfcfaaca
Sam Deane <sam@elegantchaos.com>
2022-02-22 14:27:42 +0000

    Added test

--- Tests/SkyrimFileFormatTests/FieldCodingTests.swift
+++ Tests/SkyrimFileFormatTests/FieldCodingTests.swift
@@ -28,4 +28,27 @@ class FieldCodingTests: ProcessorTestCase {
         let encoded = try encoder.encode(decoded)
         XCTAssertEqual(data, encoded)
     }
+    
+    func testAlternateTextures() throws {
+        let data = Data([
+            0x01, 0x00, 0x00, 0x00, // count 1
+            0x04, 0x00, 0x00, 0x00, // size 4
+            0x54, 0x65, 0x73, 0x74, // "Test"
+            0x01, 0x02, 0x03, 0x04, // FormID 0x04030201
+            0x0F, 0x00, 0x00, 0x00  // 3d index 0x0F
+        ])
+        
+        let decoder = DataDecoder(data: data)
+        let decoded = try decoder.decode(AlternateTextureField.self)
+        XCTAssertEqual(decoded.textures.count, 1)
+        let texture = decoded.textures.first!
+        XCTAssertEqual(texture.name, "Test")
+        XCTAssertEqual(texture.texture.id, 0x04030201)
+        XCTAssertEqual(texture.index, 0xF)
+        XCTAssertEqual(decoder.remainingCount(), 0)
+        
+        let encoder = DataEncoder()
+        let encoded = try encoder.encode(decoded)
+        XCTAssertEqual(data, encoded)
+    }
 }
