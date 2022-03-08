e756692968504ef687deaa395a26fd04ef8d2c66
Sam Deane <sam@elegantchaos.com>
2022-03-08 12:30:36 +0000

    more cleanup

--- Sources/SkyrimFileFormat/Functions.swift
+++ Sources/SkyrimFileFormat/Functions.swift
@@ -6,14 +6,22 @@
 import Foundation
 
 enum ArgType {
-    case actor(String = "")
-    case container(String = "")
-    case integer(String = "")
-    case string(String = "")
-    case float(String = "")
-    case topic(String = "")
-    case quest(String = "")
+    case actor(String? = nil)
+    case container(String? = nil)
+    case integer(String? = nil)
+    case string(String? = nil)
+    case float(String? = nil)
+    case topic(String? = nil)
+    case quest(String? = nil)
     case unknown(Int, String)
+    
+    static var integer: Self { .integer()}
+    static var float: Self { .float()}
+    static var string: Self { .string()}
+    static var actor: Self { .actor()}
+    static var container: Self { .container()}
+    static var topic: Self { .topic() }
+    static var quest: Self { .quest()}
 }
 
 //struct Function: RawRepresentable, BinaryCodable {
@@ -25,10 +33,16 @@ enum ArgType {
 struct Function {
     let id: Int
     let name: String
+    let description: String?
+    let abbreviation: String?
+    let arguments: [ArgType]
     
-    init(_ id: Int, ref: Bool = false, _ name: String, _ args: ArgType..., description: String? = nil) {
+    init(_ id: Int, ref: Bool = false, _ name: String, abbrev abbreviation: String? = nil, _ args: ArgType..., description: String? = nil) {
         self.id = id
         self.name = name
+        self.abbreviation = abbreviation
+        self.description = description
+        self.arguments = args
     }
 }
 
